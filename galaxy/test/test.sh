ac874583459634be667c0fb94e1ac5f5e3bb6136
Olivier Coen <coen.olivier@gmail.com>
2025-07-10 10:06:00 +0200

    update

--- galaxy/test/test.sh
+++ galaxy/test/test.sh
@@ -4,5 +4,5 @@ galaxy_dir="$(dirname $(dirname $(readlink -f "$0")))"
 tool_file="${galaxy_dir}/tool/nf_core_stableexpression.xml"
 
 # add --update_test_data to create output file
-planemo test $tool_file
+planemo test $tool_file --update_test_data
 
