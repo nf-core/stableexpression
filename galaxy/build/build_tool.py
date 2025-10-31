d591b37110a0d8e9633a9c4f90928d0df306c0f6
Olivier Coen <coen.olivier@gmail.com>
2025-10-31 08:31:57 +0100

    update galaxy tooling for first publishing of downloadable galaxy tool

--- galaxy/build/build_tool.py
+++ galaxy/build/build_tool.py
@@ -9,7 +9,7 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 tool_boilerplate_file = Path(__file__).parent / "static/boilerplate.xml"
-tool_file = Path(__file__).parents[1] / "tool/nf_core_{}.xml"
+tool_file = Path(__file__).parents[1] / "tool_shed/tool/nf_core_{}.xml"
 
 
 def main():
