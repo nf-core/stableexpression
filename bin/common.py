e4f8e2f29a97f9165102304fb5814aa4ad2fbbbf
Olivier <coen.olivier@gmail.com>
2026-01-30 00:22:48 +0100

    fix issue of NA not recognised when parsing count tables

--- bin/common.py
+++ bin/common.py
@@ -33,7 +33,12 @@ def parse_table(file: Path):
         sep = "," if file.suffix == ".csv" else "\t"
         header = parse_header(file, sep)
         return pl.read_csv(
-            file, separator=sep, has_header=False, skip_rows=1, new_columns=header
+            file,
+            separator=sep,
+            has_header=False,
+            skip_rows=1,
+            new_columns=header,
+            null_values=["NA", "N/A", "na", "n/a"],
         )
     elif file.suffix == ".parquet":
         return pl.read_parquet(file)
