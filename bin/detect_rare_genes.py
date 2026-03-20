2adc3b4193e6c2d8c49b6350478f4c6b7fd9f7b1
Olivier Coen <coen.olivier@gmail.com>
2026-03-20 07:48:50 +0100

    sort dataframe for output consistency

--- bin/detect_rare_genes.py
+++ bin/detect_rare_genes.py
@@ -106,10 +106,13 @@ def main():
         .unique()
     )
 
+    # selecting columns for output and sorting (for output consistency)
+    df = df.select([config.GENE_ID_COLNAME, "total_occurrences_quantile"]).sort(
+        ["total_occurrences_quantile", "gene_id"], descending=[True, False]
+    )
+
     # writing total occurrences in a csv before filtering
-    df.select([config.GENE_ID_COLNAME, "total_occurrences_quantile"]).sort(
-        "total_occurrences_quantile", descending=True
-    ).write_csv(TOTAL_OCCURRENCES_OUTFILE)
+    df.write_csv(TOTAL_OCCURRENCES_OUTFILE)
 
     # filtering genes
     valid_gene_ids = (
